72b651ed5736fab4e556283dd8ced87dfb8428e2
winner00 <TOverturf@gmail.com>
2014-02-04 23:40:22 -0700

    Add boot animation
    Change-Id: I6c28877af3a2e5e3ccfd79ebf07ab14a81163e7e

--- carbon.mk
+++ carbon.mk
@@ -18,6 +18,10 @@
 # name
 PRODUCT_RELEASE_NAME := Nexus7LTE
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
 # device
 $(call inherit-product, device/asus/deb/aosp_deb.mk)
 
